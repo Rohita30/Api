3beb8a7c03cb854bea0b5bd07d9185a4f0de0c1a
Rohita30 <rohitap938@gmail.com>
2023-07-11 12:00:54 +0530

    commit

---
+++ src/main/java/com/gateway/MicroserviceApiApplication.java
@@ -0,0 +1,14 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApiApplication.class, args);
+	}
+
+}
